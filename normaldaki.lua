7bf044bbbce6336223352319bcbf9f7e06c9479a
Liddex2011 <ilnarsaetgalin4@gmail.com>
2026-01-08 20:42:35 +0500

    Create normaldaki.lua

---
+++ normaldaki.lua
@@ -0,0 +1,39 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Коды от великого Ильнара", "RJTheme3")
+
+local Tab = Window:NewTab("Infinity Yeild")
+
+local Section = Tab:NewSection("Нормас")
+
+Section:NewButton("Открыть инфинити", "Открывает инфинити", function()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+   end)
+
+   local Tab1 = Window:NewTab("Полезные функции")
+
+   local Section1 = Tab1:NewSection("Нормалдаки")
+
+Section1:NewSlider("SpeedHack", "Изменяет вашу скорость от 0 до 500", 500, 0, function(s) -- 500 макс | 0 минималка
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+    end)
+
+Section1:NewSlider("JumpPower", "Увеличивает ваш прыжок от 0 до 500", 500, 0, function(s) 
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+ Section1:NewButton("Открыть меню NoClip", "Открывается менюшка с ноуклипом", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/2JZWpiKn", true))()
+end)
+
+Section1:NewButton("Открыть меню fly", "Открывается менюшка с полетом", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+end)
+
+local Tab2 = Window:NewTab("Веселье и разьеб карты")
+
+local Section2 = Tab2:NewSection("Это халяль")
+
+Section2:NewButton("Natural Disaster", "есть функция, которая тянет все блоки к себе", function()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/zeroidxx/axe-hub/refs/heads/main/axehub%20nds.txt'))()
+  end)
